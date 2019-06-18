2d6d2b0edebd9c71460bd678dcde1c4e585ae279
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-18 18:18:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,7 +26,7 @@ def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.each do |element|
-    if element.include?(cheese_types.to_s)
+    if cheese_types.include?(element)
       return "#{element}"
     end
   end
