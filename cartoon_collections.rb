8ff9d275627427b3f2855602fc6145a38a479dfa
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-18 18:09:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,14 @@ def summon_captain_planet(array)# code an argument here
   end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  array.each do |call|
+    if call.size > 4
+      return true
+      break
+    end
+  end
 end
 
 def find_the_cheese# code an argument here
