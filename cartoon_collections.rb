e8da2874c1e5a118886c6b870d3621897746eb0a
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-18 18:09:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,8 @@ def long_planeteer_calls(array)# code an argument here
   array.each do |call|
     if call.size > 4
       return true
-      break
+    else
+      return false
     end
   end
 end
